3a313cee678e55289b24f4b66996f652f71ef794
weck <weck2333@gmail.com>
2019-03-11 09:09:46 +0800

    making authenticated requests

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,36 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
-void mian() => runApp(MyApp());
+import 'package:http/http.dart' as http;
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final title = 'Gesture Demo';
-    return MaterialApp(
-      title: title,
-      home: MyHomePage(title: title),
-    );
+Future<Post> fetchPost() async {
+  final response = await http.get(
+    'https://jsonplaceholder.typicode.com/posts/1',
+    headers: {
+      HttpHeaders.authorizationHeader:'Basic your_api_token_here'
+    },
+  );
+  final responseJson =json.decode(response.body);
+
+  return Post.fromJson(responseJson);
   }
-}
+  
+  class Post {
+    final int userId;
+    final int id;
+    final String title;
+    final String body;
+    
+    Post({this.userId, this.id, this.title, this.body});
+  
+    factory Post.fromJson(Map<String,dynamic> json) {
+      return Post(
+        userId: json['userId'],
+        id: json['id'],
+        title: json['title'],
+        body: json['body'],
+      );
+    }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(child: MyButton(),),
-    );
-  }
-}
-
-class MyButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        final snackBar =SnackBar(content: Text('Tap'),);
-
-        Scaffold.of(context).showSnackBar(snackBar);
-      },
-
-      child: Container(
-        padding: EdgeInsets.all(12.0),
-        decoration: BoxDecoration(
-          color: Theme.of(context).buttonColor,
-          borderRadius: BorderRadius.circular(8.0),
-        ),
-        child: Text('My Button'),
-      ),
-    );
-  }
 }
